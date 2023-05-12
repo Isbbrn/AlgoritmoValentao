c4756a74ea7aeb0f7064b19323f735aacede7f08
Isbbrn <30944679+Isbbrn@users.noreply.github.com>
2023-05-12 19:28:24 -0300

    Add files via upload

--- algoritmo.py
+++ algoritmo.py
@@ -1,58 +1,107 @@
+import socket
+import threading
+
 class Processo:
-    def __init__(self, id):
+    def __init__(self, id, endereco):
         self.id = id
+        self.endereco = endereco
         self.esta_ativo = True
         self.lider = None
 
-    def enviar_mensagem(self, processo):
-        if processo.esta_ativo:
-            print(f"Processo {self.id} envia mensagem para processo {processo.id}")
-        else:
-            print(f"Processo {self.id} não pode enviar mensagem para processo {processo.id} porque está inativo")
+    def enviar_mensagem(self, endereco_destino):
+        if not self.esta_ativo:
+            return
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(endereco_destino)
+            mensagem = f"{self.id}:ELEICAO"
+            sock.sendall(mensagem.encode())
+            sock.close()
+            print(f"Processo {self.id} enviou mensagem para {endereco_destino}")
+        except:
+            print(f"Processo {self.id} não pode enviar mensagem para {endereco_destino}")
 
-    def receber_mensagem(self, processo):
-        if processo.esta_ativo:
-            print(f"Processo {self.id} recebe mensagem de processo {processo.id}")
-            if processo.lider is not None:
-                print(f"Processo {self.id} detecta que processo {processo.lider.id} é o líder")
-                self.lider = processo.lider
-        else:
-            print(f"Processo {self.id} não pode receber mensagem de processo {processo.id} porque está inativo")
+    def receber_mensagem(self, mensagem):
+        if not self.esta_ativo:
+            return
+        remetente, conteudo = mensagem.split(":")
+        print(f"Processo {self.id} recebeu mensagem de {remetente}: {conteudo}")
+        if conteudo == "ELEICAO":
+            self.lider = None
+            for processo in processos:
+                if processo.id > self.id:
+                    processo.enviar_mensagem(processo.endereco)
+                elif processo.id < self.id:
+                    self.enviar_mensagem(processo.endereco)
+            self.verificar_lider()
 
-    def iniciar_eleicao(self, processos):
+    def iniciar_eleicao(self):
         if not self.esta_ativo:
             return
         print(f"Processo {self.id} inicia eleição")
         self.lider = self
         for processo in processos:
             if processo.id > self.id:
-                self.enviar_mensagem(processo)
-                self.receber_mensagem(processo)
-                if self.lider != self:
-                    break
+                self.enviar_mensagem(processo.endereco)
+                break
+        self.verificar_lider()
+
+    def verificar_lider(self):
+        if not self.esta_ativo:
+            return
+        for processo in processos:
+            if processo.lider is None:
+                return
+            elif processo.lider.id > self.lider.id:
+                self.lider = processo.lider
         if self.lider == self:
             print(f"Processo {self.id} é o líder")
             for processo in processos:
-                if processo.id < self.id:
-                    processo.enviar_mensagem(self)
-                    processo.receber_mensagem(self)
+                if processo.id != self.id:
+                    processo.enviar_mensagem(processo.endereco)
 
     def falhar(self):
         self.esta_ativo = False
         if self.lider == self:
             for processo in processos:
                 if processo.id > self.id:
-                    processo.iniciar_eleicao(processos)
+                    processo.iniciar_eleicao()
                     break
         else:
             self.lider = None
 
+class Servidor:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(self.endereco)
+        self.sock.listen(1)
+        self.thread = threading.Thread(target=self.ouvir)
+        self.thread.start()
+
+    def ouvir(self):
+        while True:
+            conexao, endereco = self.sock.accept()
+            mensagem = conexao.recv(1024)
+            for processo in processos:
+                if processo.endereco == endereco:
+                    processo.receber_mensagem(mensagem.decode())
+                    break
+            conexao.close()
+
+processos = [Processo(1, ('localhost', 5000)),
+Processo(2, ('localhost', 5001)),
+Processo(3, ('localhost', 5002))]
+
+servidor_5000 = Servidor(('localhost', 5000))
+servidor_5001 = Servidor(('localhost', 5001))
+servidor_5002 = Servidor(('localhost', 5002))
 
 if __name__ == "__main__":
-    p1 = Processo(1)
-    p2 = Processo(2)
-    p3 = Processo(3)
-    p4 = Processo(4)
+    p1 = Processo(1, ('localhost', 5000))
+    p2 = Processo(2, ('localhost', 5001))
+    p3 = Processo(3, ('localhost', 5002))
+    p4 = Processo(4, ('localhost', 5003))
     processos = [p1, p2, p3, p4]
 
     p2.falhar()
